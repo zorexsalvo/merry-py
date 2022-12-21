9a887fc991859a63fa6bdfaa14a9b1c47d6bfef8
Zorex <zorex@brokerengine.com.au>
2022-12-21 18:47:57 +0800

    Modified the chords, use a single note only for pitch accuracy

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 from musicpy import *
 
-
 # We wish you a merry christmas
-first_line = (C('C') |
-              C('F') |
-              C('F', duration=1/8) |
-              C('G', duration=1/8) |
-              C('F', duration=1/8) |
-              C('Em', duration=1/8) |
-              C('D') |
-              C('D'))
+c = chord("C4")
+c_18 = chord("C4", duration=1/8)
+f = chord("F4")
+f_18 = chord("F4", duration=1/8)
+g = chord("G4")
+g_18 = chord("G4", duration=1/8)
+e = chord("E4")
+e_18 = chord("E4", duration=1/8)
+d = chord("D4")
+a = chord("A4")
+a_18 = chord("A4", duration=1/8)
+am = chord("A#4")
+am_18 = chord("A#4", duration=1/8)
 
-second_line = (C('D') |
-               C('G') |
-               C('G', duration=1/8) |
-               C('Am', duration=1/8) |
-               C('G', duration=1/8) |
-               C('F', duration=1/8) |
-               C('Em') |
-               C('Em'))
+first_line = (c | f | f_18 | g_18 | f_18 | e_18 | d | d)
+second_line = (d | g | g_18 | a_18 | g_18 | f_18 | e | e )
+third_line = (e | a | a_18 | am_18 | a_18 | g_18 | f | d)
+last_line = (c_18)
 
-third_line = (C('Em') |
-              C('Am') |
-              C('Am', duration=1/8) |
-              C('A#m', duration=1/8) |
-              C('Am', duration=1/8) |
-              C('G', duration=1/8) |
-              C('F') |
-              C('D'))
 
 last_line = (C('C', duration=1/8) |
              C('C', duration=1/8) |
